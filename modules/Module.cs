035d95d4b11c4e9ce1e3735e0aedced677edf5f0
Your Name <you@example.com>
2022-05-28 22:29:07 -0400

    kill command is working

--- modules/Module.cs
+++ modules/Module.cs
@@ -1,12 +1,24 @@
-﻿using Discord.Commands;
+﻿using CustomLogging;
+using Discord;
+using Discord.Commands;
 
 namespace KillBot.modules
 {
     public class Module: ModuleBase<SocketCommandContext>
     {
+        private readonly ILogger logger;
 
-        [Command("say")]
-        public Task SayAsync([Remainder] [Summary("The text to echo")] string str) => ReplyAsync(str);
+        public Module(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        [Command("kill")]
+        public async Task KillAsync(IUser user)
+        {
+            logger.Verbose("Kill command activated");
+            await ReplyAsync($"☠ User: {user.Username} ☠");
+        }
 
     }
 }
