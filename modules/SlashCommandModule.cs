cdf60125f4e0afe977e39a23728ed142f9481af4
Tyler Dalbora <tylerdalbora@gmail.com>
2024-09-20 20:41:24 -0400

    testing 2

---
+++ modules/SlashCommandModule.cs
@@ -0,0 +1,18 @@
+using System.Reflection.Emit;
+using Discord;
+using Discord.Interactions;
+namespace KillBot.modules
+{
+    public class SlashCommandModule : InteractionModuleBase<IInteractionContext>
+    {
+
+
+        public SlashCommandModule():base(){
+
+        }
+
+        // protected override void OnModuleBuilding(CommandService commandService, ModuleBuilder builder){
+
+        // }
+    }
+}
